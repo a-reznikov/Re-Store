4c92f741b5c1af2ce072559dc0735ebbe70243f0
a-reznikov <a.reznnikov@gmail.com>
2023-10-11 17:38:44 +0300

    feat: add routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 import './app.css'
-import withBookstoreService from "../hoc";
+import { HomePage, CartPage } from '../pages'
 
-const App = ({ bookstoreService }) => {
-
-  console.log(bookstoreService.getBooks());
+const App = () => {
 
   return (
     <div className="app">App
-
+      <Link to='/'>Home</Link>
+      <Link to='/cart/'>Cart</Link>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/cart/' element={<CartPage />} />
+      </Routes>
     </div>
   )
 }
 
-export default withBookstoreService()(App);
+export default App;
